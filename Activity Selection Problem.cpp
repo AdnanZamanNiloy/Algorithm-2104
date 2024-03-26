252a2219c9b1b442753f822544411a8addee3399
Adnan Zaman <156665143+AdnanZamanNiloy@users.noreply.github.com>
2024-03-26 12:08:01 +0600

    Update Activity Selection Problem.cpp

--- Activity Selection Problem.cpp
+++ Activity Selection Problem.cpp
@@ -1,3 +1,8 @@
+/*Statement: You are given n activities with their start and finish times.
+ Select the maximum number of activities that can be performed by a single
+ person, assuming that a person can only work on a single activity at a time.   
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
